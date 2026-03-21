55326ade4506605a3a2aa545e8e1bfc8f3c709cb
atharvaO4 <atharvajagdale104@gmail.com>
2026-03-21 11:44:53 +0530

    struct file basic done

--- structs.h
+++ structs.h
@@ -1,4 +1,49 @@
 #ifndef STRUCTS_H
 #define STRUCTS_H
 
+#define MAX_STUDENTS 50
+#define MAX_TEACHERS 10
+#define MAX_ASSIGNMENT 50
+#define MAX_ANNOUNCEMENTS 50
+#define MAX_QUIZ 50
+#define MAX_OPTIONS 4
+
+#define FILE_STUDENTS      "data/students.dat"
+#define FILE_TEACHERS      "data/teachers.dat"
+#define FILE_ASSIGNMENTS   "data/assignments.dat"
+#define FILE_ANNOUNCEMENTS "data/announcements.dat"
+#define FILE_QUIZ          "data/quiz.dat"
+
+typedef struct {
+    int id;
+    char name[50];
+    int totalClasses;
+    int attendedClasses;
+    float assignmentMarks;
+    float quizMarks;
+} Student;
+
+typedef struct {
+    int id;
+    char name[50];
+} Teacher;
+
+typedef struct {
+    int id;
+    char subject[50];
+    char title[100];
+    char deadline[20];
+} Assignment;
+
+typedef struct {
+    int id;
+    char messsage[200];
+} Announcement;
+
+typedef struct {
+    int id;
+    char question[200];
+    char option[MAX_OPTIONS][100];
+    int correctoption;
+} quiz;
 #endif 
