c198b4c7b003d17c5d1942aa6c893db87bddaa31
yogeshsikhwal77 <yogeshsikhwal77@gmail.com>
2026-03-28 23:00:25 +0530

    crate student.h and function for student module

---
+++ student.h
@@ -0,0 +1,10 @@
+#ifndef STUDENT_H
+#define STUDENT_H
+
+void student_menu(int studentId);
+void view_attendance(int studentId);
+void view_assignments(int studentId);
+void view_grades(int studentId);
+void view_announcements(void);
+
+#endif
